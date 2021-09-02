d9c17fec4d8ddb205630d68106efb6f27c4b9d53
Chloe Schoepke <chloe.schoepke@my.simpson.edu>
2021-09-02 15:14:48 -0500

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("I am writing print statements.")
+print("Since I am feeling fancy I am going to add in a quote I find fitting.")
+print("\"If you judge a fish by its ability to climb a tree, it will live its whole life believing it is stupid\"")
+print("This is an accurate description of my feelings towards my current coding abilities being judged.")
