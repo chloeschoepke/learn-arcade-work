2caa10a05bb5c128f484c07b181d0eca3963b69d
Chloe Schoepke <chloe.schoepke@my.simpson.edu>
2021-11-10 12:23:51 -0600

    Finish lab 9

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -1,15 +1,17 @@
-
-
 import arcade
+import random
 
 # --- Constants ---
 SPRITE_SCALING_BOX = 0.5
 SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.2
+COIN_COUNT = 50
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 MOVEMENT_SPEED = 5
+CAMERA_SPEED = 1
 
 
 class MyGame(arcade.Window):
@@ -20,13 +22,18 @@ class MyGame(arcade.Window):
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
+        # Load coin sound
+        self.coin_sound = arcade.load_sound("coin1.wav")
+
         # Sprite lists
         self.player_list = None
         self.wall_list = None
         self.rock_list = None
+        self.coin_list = None
 
         # Set up the player
         self.player_sprite = None
+        self.score = 0
 
         # This variable holds our simple "physics engine"
         self.physics_engine = None
@@ -45,17 +52,20 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.rock_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
 
         # Reset the score
         self.score = 0
 
         # Create the player
+        # Character image from kenney.nl
         self.player_sprite = arcade.Sprite("character1.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 64
         self.player_list.append(self.player_sprite)
 
         # --- Place boxes inside a loop
+        # Stone wall and rock image from kenney.nl
         for x in range(0, 1024, 64):
             wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
             wall.center_x = x
@@ -92,27 +102,94 @@ class MyGame(arcade.Window):
             wall.center_y = 140
             self.wall_list.append(wall)
 
-        for x in range(64, 500, 64):
+        for x in range(64, 700, 64):
             wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
             wall.center_x = x
             wall.center_y = 280
             self.wall_list.append(wall)
 
-        coordinate_list = [[450, 210],
-                           [470, 500],
-                           [480, 240]]
+        for x in range(120, 300, 64):
+            wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 420
+            self.wall_list.append(wall)
+
+        for x in range(400, 800, 64):
+            wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 420
+            self.wall_list.append(wall)
+
+        for x in range(250, 800, 64):
+            wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 560
+            self.wall_list.append(wall)
+
+        for x in range(64, 750, 64):
+            wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 700
+            self.wall_list.append(wall)
+
+        # Individually place rocks using coordinates
+        rock_coordinate_list = [[450, 210],
+                                [470, 500],
+                                [480, 240],
+                                [896, 420],
+                                [855, 445],
+                                [120, 800],
+                                [700, 800],
+                                [400, 900],
+                                [270, 900]]
 
         # Loop through coordinates
-        for coordinate in coordinate_list:
+        for coordinate in rock_coordinate_list:
             rock = arcade.Sprite("rock.png", SPRITE_SCALING_BOX)
             rock.center_x = coordinate[0]
             rock.center_y = coordinate[1]
             self.wall_list.append(rock)
 
+        # Individually place walls using coordinates
+        wall_coordinate_list = [[896, 700],
+                                [832, 700]]
+
+        # Loop through coordinates
+        for coordinate in wall_coordinate_list:
+            wall = arcade.Sprite("stone.png", SPRITE_SCALING_BOX)
+            wall.center_x = coordinate[0]
+            wall.center_y = coordinate[1]
+            self.wall_list.append(wall)
+
         # Create the physics engine. Give it a reference to the player, and
         # the walls we can't run into.
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
+        for i in range(COIN_COUNT):
+            # Create the coin instance
+            # Coin image from kenney.nl
+            coin = arcade.Sprite("coin.png", SPRITE_SCALING_COIN)
+
+            coin_placed_successfully = False
+
+            # Keep trying until success
+            while not coin_placed_successfully:
+                # Position the coin
+                coin.center_x = random.randrange(960)
+                coin.center_y = random.randrange(960)
+
+                # See if the coin is hitting a wall
+                wall_hit_list = arcade.check_for_collision_with_list(coin, self.wall_list)
+
+                # See if the coin is hitting another coin
+                coin_hit_list = arcade.check_for_collision_with_list(coin, self.coin_list)
+
+                if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
+                    # It is!
+                    coin_placed_successfully = True
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
 
     def on_draw(self):
         arcade.start_render()
@@ -124,6 +201,7 @@ class MyGame(arcade.Window):
         self.wall_list.draw()
         self.player_list.draw()
         self.rock_list.draw()
+        self.coin_list.draw()
 
         # Select the (unscrolled) camera for our GUI
         self.camera_for_gui.use()
@@ -132,16 +210,16 @@ class MyGame(arcade.Window):
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
         self.physics_engine.update()
 
-        # Scroll the window to the player.
-        #
-        # If CAMERA_SPEED is 1, the camera will immediately move to the desired position.
-        # Anything between 0 and 1 will have the camera move to the location with a smoother
-        # pan.
-        CAMERA_SPEED = 1
+        self.coin_list.update()
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.coin_list)
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+            arcade.play_sound(self.coin_sound)
+
         lower_left_corner = (self.player_sprite.center_x - self.width / 2,
                              self.player_sprite.center_y - self.height / 2)
         self.camera_for_sprites.move_to(lower_left_corner, CAMERA_SPEED)
