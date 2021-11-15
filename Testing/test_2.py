e676fb5ebc02edc3803697b449483b13f37c8226
Chloe Schoepke <chloe.schoepke@my.simpson.edu>
2021-11-15 12:28:21 -0600

    platformer

--- Testing/test_2.py
+++ Testing/test_2.py
@@ -1,21 +1,2 @@
-import random
 
-
-my_number = random.randrange(5)
-if my_number == 0:
-    print("Dragon!")
-else:
-    print("No Dragon.")
-
-    thirst = 0
-    hydroflask = 3
-
-    elif choice.upper == "A":
-    if hydroflask > 0:
-        hydroflask -= 1
-        thirst = 0
-        print("Way to hydrate!")
-    else
-        print("you have die-drated, should have hydrated instead")
-    break
 
