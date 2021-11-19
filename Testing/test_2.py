6c1b7165d72234dbbb843e0aa005e73391632835
Chloe Schoepke <chloe.schoepke@my.simpson.edu>
2021-11-19 12:19:50 -0600

    class notes

--- Testing/test_2.py
+++ Testing/test_2.py
@@ -1,11 +1,19 @@
+
+def selection_sort(my_list):
+    for cur_pos in range(len(my_list)):
+        min_pos = cur_pos
+        for scan_pos in range(cur_pos + 1, len(my_list)):
+            if my_list[scan_pos] < my_list[min_pos]:
+                min_pos = scan_pos
+
+        # Swap
+        temp = my_list[min_pos]
+        my_list[min_pos] = my_list[cur_pos]
+        my_list[cur_pos] = temp
+
+
 my_list = [15, 57, 14, 33, 72, 79, 26, 56, 42, 40]
-print (my_list)
-# swap the 15 and 14 values
-temp = my_list[2]
-my_list[2] = my_list[0]
-my_list[0] = temp
-
-
+selection_sort(my_list)
 print(my_list)
 
-
+# n * (n / 2) = n^2 / 2
